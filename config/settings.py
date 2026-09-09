84f47ca76e87a4c17de0fca988494908ba249fb0
PjnSai <137239481+PjnSai@users.noreply.github.com>
2026-09-09 19:40:55 +0100

    Connect React frontend to Django API via CORS - end-to-end data flow working

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'boards',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
