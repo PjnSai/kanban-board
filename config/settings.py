870839bd193a5e961d6ceb62c20e9df985fcc81b
PjnSai <137239481+PjnSai@users.noreply.github.com>
2026-09-26 02:48:47 +0100

    Switch from SQLite to Postgres (Neon)

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 from corsheaders.defaults import default_headers
 from datetime import timedelta
+import dj_database_url
+
 
 SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
@@ -126,11 +128,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
 
 
 # Password validation
